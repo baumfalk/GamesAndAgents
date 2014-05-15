271a422e29dfc78125906c22d3132644dbb17689
baumfalk <j.t.baumfalk@gmail.com>
2014-05-15 10:52:47 +0200

    allows for nice training

--- CaptureTheFlag/train.py
+++ CaptureTheFlag/train.py
@@ -1,7 +1,21 @@
 import os
-cmd = "simulate.py match -a -v debug SleekoCommander.SleekoCommander barriebot.BarrieCommander -l map53"
+cmdBasic = "simulate.py match -a -v debug -l map53"
 
-for i in range(10):
-    print i
-    os.system(cmd)
+ourBot = "barriebot.BarrieCommander" # {packagename}.{botname}
+
+showdowns = {} #nameofbot (str) / numberofbattles (int)
+showdowns["examples.RandomCommander"] = 10
+showdowns["examples.GreedyCommander"] = 11
+showdowns["examples.DefenderCommander"] = 12
+showdowns["examples.BalancedCommander"] = 13
+showdowns["mycmd.PlaceholderCommander"] = 12
+showdowns["SleekoCommander.SleekoCommander"] = 12
+                                        
+for key in showdowns.keys():
+    print "Battling ", ourBot , " against ", key, "!"
+    for i in range(showdowns[key]):
+        print "Match [",i,"/",showdowns[key],"]"
+        cmd = cmdBasic + " " + ourBot + " " + key + " --headless"
+        print  cmd
+        os.system(cmd)
     
