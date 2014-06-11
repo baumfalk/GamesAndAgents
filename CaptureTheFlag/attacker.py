8c636b9a714f43cf882f21151de3a54796b42983
baumfalk <j.t.baumfalk@gmail.com>
2014-06-11 11:41:56 +0200

    Added documentation

--- CaptureTheFlag/attacker.py
+++ CaptureTheFlag/attacker.py
@@ -2,22 +2,24 @@ from api import orders
 
 #General attack rules.
 
+
+def attack_rule_1(bot,commander,knowledgeBase):
 """
 1. [3] if (our flag captured && at midsection)
     Charge enemy base
 """
-def attack_rule_1(bot,commander,knowledgeBase):
     if(knowledgeBase.ourFlagCaptured and knowledgeBase.atMidsection(bot)):
         commander.issue(orders.Charge, bot, knowledgeBase.enemyBase,description = "Attacker" + bot.name + "charge to enemy flag")
        # knowledgeBase.updateStatistics(things)
         return True
     return False
 
+
+def attack_rule_2(bot,commander,knowledgeBase):
 """
 2. [3] if (our flag captured && at midsection)
     Charge closest side edges
 """  
-def attack_rule_2(bot,commander,knowledgeBase):
     if(knowledgeBase.ourFlagCaptured and knowledgeBase.atMidsection(bot)):
         loc = knowledgeBase.nearestSideEdge(bot)
         commander.issue(orders.Charge, bot, loc,description = "Attacker" + bot.name + "charge to nearest side edge")
@@ -25,22 +27,25 @@ def attack_rule_2(bot,commander,knowledgeBase):
         return True
     return False
 
+
+def attack_rule_3(bot,commander,knowledgeBase):
 """
 3. [3] if (our flag captured && not at midsection)
     Charge at midsection
 """
-def attack_rule_3(bot,commander,knowledgeBase):
     if(knowledgeBase.ourFlagCaptured and not knowledgeBase.atMidsection(bot)):
         loc = knowledgeBase.getMidsection(bot)
         commander.issue(orders.Attack, bot, loc,description = "Attacker" + bot.name + "move to mid section")
        # knowledgeBase.updateStatistics(things)
         return True
     return False    
+
+	
+def attack_rule_4(bot,commander,knowledgeBase):
 """
 4. [3] if (our flag captured && not at midsection)
     Charge (atclosest side edges)
 """
-def attack_rule_4(bot,commander,knowledgeBase):
     if(knowledgeBase.ourFlagCaptured and not knowledgeBase.atMidsection(bot)):
         loc = knowledgeBase.nearestSideEdge(bot)
         commander.issue(orders.Charge, bot, loc,description = "Attacker" + bot.name + "charge to nearest side edge")
@@ -49,11 +54,11 @@ def attack_rule_4(bot,commander,knowledgeBase):
     return False
 
 
+def attack_rule_5(bot,commander,knowledgeBase):
 """
 5. [2] if (flag picked up by enemy)
     Charge towards enemyCarrier position.
 """
-def attack_rule_5(bot,commander,knowledgeBase):
     if(knowledgeBase.ourFlagCaptured):
         loc = knowledgeBase.predictPosition(commander.game.team.flag.carrier)
         commander.issue(orders.Charge, bot, loc,description = "Attacker" + bot.name + "charge to enemy carrier")
@@ -61,11 +66,12 @@ def attack_rule_5(bot,commander,knowledgeBase):
         return True
     return False
 
+
+def attack_rule_6(bot,commander,knowledgeBase):
 """
 6. [2] if (our flag is not picked up)
     Charge to enemy flag
 """
-def attack_rule_6(bot,commander,knowledgeBase):
     if( not knowledgeBase.ourFlagCaptured):
         loc = knowledgeBase.enemyBase
         print "attack rule 6: ", loc
@@ -74,11 +80,12 @@ def attack_rule_6(bot,commander,knowledgeBase):
         return True
     return False
 
+
+def attack_rule_7(bot,commander,knowledgeBase):
 """
 7. [0] if (our flag is in base && not in area near the midsection)
     Move (to the midsection)
 """
-def attack_rule_7(bot,commander,knowledgeBase):
     if(knowledgeBase.flagInBase and not knowledgeBase.atMidsection(bot)):
         loc = knowledgeBase.getMidsection(bot)
         commander.issue(orders.Attack, bot, loc,description = "Attacker" + bot.name + "moving towards midsection")
@@ -86,11 +93,12 @@ def attack_rule_7(bot,commander,knowledgeBase):
         return True
     return False
 
+
+def attack_rule_8(bot,commander,knowledgeBase):
 """
 8. [0] if ( I'm closest to our flag carrier)
     Move (To our flag carrier) 
 """
-def attack_rule_8(bot,commander,knowledgeBase):
     ourFlagCarrierPos = knowledgeBase.teamNearestFriend(bot).position
     if(  ourFlagCarrierPos == commander.game.enemyTeam.flag.position):
         commander.issue(orders.Attack, bot, ourFlagCarrierPos,description = "Attacker" + bot.name + "moving towards our flag carrier")
@@ -98,11 +106,12 @@ def attack_rule_8(bot,commander,knowledgeBase):
         return True
     return False
 
+
+def attack_rule_9(bot,commander,knowledgeBase):
 """
 9. [0] if ( I'm closest to our flag carrier)
     Charge (To our flag carrier) 
 """
-def attack_rule_9(bot,commander,knowledgeBase):
     ourFlagCarrierPos = knowledgeBase.teamNearestFriend(bot).position
     if(  ourFlagCarrierPos == commander.game.enemyTeam.flag.position):
         commander.issue(orders.Charge, bot, ourFlagCarrierPos,description = "Attacker" + bot.name + "Charging towards our flag carrier")
@@ -111,11 +120,12 @@ def attack_rule_9(bot,commander,knowledgeBase):
     return False  
 
 
+
+def attack_rule_10(bot,commander,knowledgeBase):
 """ 
 10.[0] if (we have their flag)
     Attack (towards our carrier along a short flanking path) #Try to intercept enemy flankers.
 """
-def attack_rule_10(bot,commander,knowledgeBase):
     if(  knowledgeBase.theirFlagCaptured ):
         ourFlagCarrierPos = commander.game.enemyTeam.flag.position
         path = knowledgeBase.createShortFlankingPath(bot.position,ourFlagCarrierPos)
@@ -125,11 +135,12 @@ def attack_rule_10(bot,commander,knowledgeBase):
     return False  
 
 
+
+def attack_rule_11(bot,commander,knowledgeBase):
 """
 11.[0] if (we have their flag)
     Attack (towards our carrier along a long flanking path) #Try to intercept enemy flankers.
 """
-def attack_rule_11(bot,commander,knowledgeBase):
     if(  knowledgeBase.theirFlagCaptured ):
         ourFlagCarrierPos = commander.game.enemyTeam.flag.position
         path = knowledgeBase.createLongFlankingPath(bot.position,ourFlagCarrierPos)
@@ -138,11 +149,12 @@ def attack_rule_11(bot,commander,knowledgeBase):
         return True
     return False  
 
+
+def attack_rule_12(bot,commander,knowledgeBase):
 """
 12.[0] if (no flags are captured)
     Attack ( flanking via short route towards enemy flag)
 """
-def attack_rule_12(bot,commander,knowledgeBase):
     if( not knowledgeBase.ourFlagCaptured and not knowledgeBase.theirFlagCaptured):
         target = commander.game.enemyTeam.flag.position
         path = knowledgeBase.createShortFlankingPath(bot.position,target)
@@ -151,11 +163,12 @@ def attack_rule_12(bot,commander,knowledgeBase):
         return True
     return False
 
+
+def attack_rule_13(bot,commander,knowledgeBase):
 """
 13.[0] if (no flags are captured)
     Attack ( flanking via long route towards enemy flag)
 """
-def attack_rule_13(bot,commander,knowledgeBase):
     if( not knowledgeBase.ourFlagCaptured and not knowledgeBase.theirFlagCaptured):
         target = commander.game.enemyTeam.flag.position
         path = knowledgeBase.createLongFlankingPath(bot.position,target)
@@ -164,11 +177,12 @@ def attack_rule_13(bot,commander,knowledgeBase):
         return True
     return False
 
+
+def attack_rule_14(bot,commander,knowledgeBase):
 """
 14.[0] if (we have their flag && i'm not the closest to our carrier)
     Attack (towards our carrier)
 """
-def attack_rule_14(bot,commander,knowledgeBase):
     ourFlagCarrierPos = knowledgeBase.teamNearestFriend(bot).position
     if(  knowledgeBase.theirFlagCaptured and not(ourFlagCarrierPos == commander.game.enemyTeam.flag.position)):
         commander.issue(orders.Attack, bot, commander.game.enemyTeam.flag.position,description = "Attacker" + bot.name + "Attacking towards our flag carrier")
@@ -176,11 +190,12 @@ def attack_rule_14(bot,commander,knowledgeBase):
         return True
     return False
 
+
+def attack_rule_15(bot,commander,knowledgeBase):
 """
 15.[0] if (True)
     Attack (move to nearest friend) # General "Stick together" rule.
 """
-def attack_rule_15(bot,commander,knowledgeBase):
     if( True ):
         nearestFriend = knowledgeBase.teamNearestFriend(bot).position
         commander.issue(orders.Attack, bot, nearestFriend,description = "Attacker" + bot.name + "Moving towards nearsest teammate")
@@ -188,10 +203,11 @@ def attack_rule_15(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def default_attacker_rule(bot,commander,knowledgeBase):
 """
 	The default attacker rule.
 """
-def default_attacker_rule(bot,commander,knowledgeBase):
     if(knowledgeBase.ourFlagCaptured and knowledgeBase.atMidsection(bot)):
         commander.issue(orders.Charge, bot, knowledgeBase.enemyBase,description = "Attacker" + bot.name + "charge to enemy flag")
     else:
--- CaptureTheFlag/catcher.py
+++ CaptureTheFlag/catcher.py
@@ -10,11 +10,13 @@ def gotoflag(listFlagLocations,listFlagReturnLocations, listVisibleEnemies,rando
             return (orders.Charge,listFlagLocations[1], "Catcher going to flag flag!")
     else: # attack a random enemy in sight
         return (orders.Attack, random.choice(listVisibleEnemies),"Catcher is attacking random enemy")
+       
+	   
+def capture_rule1(bot,commander,knowledgeBase):
 """
 1. [1] if (has flag)
 	Move (closest edge map and approach scoring point following the edge)
-"""        
-def capture_rule1(bot,commander,knowledgeBase):
+""" 
     if bot.flag:
         target = commander.game.team.flagScoreLocation
         path = knowledgeBase.createShortFlankingPath(bot.position,target)
@@ -22,11 +24,12 @@ def capture_rule1(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule2(bot,commander,knowledgeBase):
 """
 2. [1] if (has flag)
 	Charge (closest edge map and approach base sides following the edge)
 """
-def capture_rule2(bot,commander,knowledgeBase):
     if bot.flag:
         target = commander.game.team.flagScoreLocation
         path = knowledgeBase.createShortFlankingPath(bot.position,target)
@@ -34,33 +37,36 @@ def capture_rule2(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule3(bot,commander,knowledgeBase):
 """
 3. [1] if (has flag)
 	Move (shortest path to base)
 """
-def capture_rule3(bot,commander,knowledgeBase):
     if bot.flag:
         target = commander.game.team.flagScoreLocation
         commander.issue(orders.Move,bot,target, description = "Catcher "+ bot.name + " Move directly to scoring point")
         return True
     return False
 	
+
+def capture_rule4(bot,commander,knowledgeBase):
 """
 4. [1] if (has flag)
 	Charge (shortest path to base)
 """
-def capture_rule4(bot,commander,knowledgeBase):
     if bot.flag:
         target = commander.game.team.flagScoreLocation
         commander.issue(orders.Charge,bot,target, description = "Catcher "+ bot.name + " Charge directly to scoring point")
         return True
     return False
 	
+
+def capture_rule5(bot,commander,knowledgeBase):
 """
 5. [1] if (has flag)
 	Move (furthest edge map and approach base sides following the edge)
 """
-def capture_rule5(bot,commander,knowledgeBase):
     if bot.flag:
         target = commander.game.team.flagScoreLocation
         path = knowledgeBase.createLongFlankingPath(bot.position,target)
@@ -68,11 +74,12 @@ def capture_rule5(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule6(bot,commander,knowledgeBase):
 """
 6. [1] if (has flag)
 	Charge (furthest edge map and approach base sides following the edge)
 """
-def capture_rule6(bot,commander,knowledgeBase):
     if bot.flag:
         target = commander.game.team.flagScoreLocation
         path = knowledgeBase.createLongFlankingPath(bot.position,target)
@@ -80,11 +87,12 @@ def capture_rule6(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule7(bot,commander,knowledgeBase):
 """ 
 7. [0] if (doesn't have flag && enemy flag isn't captured)
 	Move (closest edge map and approach flag following the side edge)
 """
-def capture_rule7(bot,commander,knowledgeBase):
     if not bot.flag and commander.game.enemyTeam.flag.carrier == None:
         target = commander.game.enemyTeam.flag.position
         path = knowledgeBase.createShortFlankingPath(bot.position,target)
@@ -92,11 +100,12 @@ def capture_rule7(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule8(bot,commander,knowledgeBase):
 """
 8. [0] if (doesn't have flag && enemy flag isn't captured)
 	Charge (closest edge map and approach flag following the side edge)
 """
-def capture_rule8(bot,commander,knowledgeBase):
     if not bot.flag and commander.game.enemyTeam.flag.carrier == None:
         target = commander.game.enemyTeam.flag.position
         path = knowledgeBase.createShortFlankingPath(bot.position,target)
@@ -104,33 +113,36 @@ def capture_rule8(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule9(bot,commander,knowledgeBase):
 """
 9. [0] if (doesn't have flag && flag isn't captured)
 	Move (closest path to flag)
 """
-def capture_rule9(bot,commander,knowledgeBase):
     if not bot.flag and commander.game.enemyTeam.flag.carrier == None:
         target = commander.game.enemyTeam.flag.position
         commander.issue(orders.Move,bot,target, description = "Catcher "+ bot.name + " Move directly to flag")
         return True
     return False
 
+
+def capture_rule10(bot,commander,knowledgeBase):
 """
 10. [0] if (doesn't have flag && enemy flag isn't captured)
 	 Charge (closest path to flag)
 """
-def capture_rule10(bot,commander,knowledgeBase):
     if not bot.flag and commander.game.enemyTeam.flag.carrier == None:
         target = commander.game.enemyTeam.flag.position
         commander.issue(orders.Charge,bot,target, description = "Catcher "+ bot.name + " Charge directly to flag")
         return True
     return False
 	
+
+def capture_rule11(bot,commander,knowledgeBase):
 """
 11. [0] if (doesn't have flag && enemy flag isn't captured)
 	 Move (furthest edge map and approach flag following the side edge)
 """
-def capture_rule11(bot,commander,knowledgeBase):
     if not bot.flag and commander.game.enemyTeam.flag.carrier == None:
         target = commander.game.enemyTeam.flag.position
         path = knowledgeBase.createLongFlankingPath(bot.position,target)
@@ -138,11 +150,12 @@ def capture_rule11(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule12(bot,commander,knowledgeBase):
 """
 12.[0] if (doesn't have flag && enemy flag isn't captured)
 	Charge (furthest edge map and approach flag following the side edge)
 """
-def capture_rule12(bot,commander,knowledgeBase):
     if not bot.flag and commander.game.enemyTeam.flag.carrier == None:
         target = commander.game.enemyTeam.flag.position
         path = knowledgeBase.createLongFlankingPath(bot.position,target)
@@ -150,11 +163,12 @@ def capture_rule12(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule13(bot,commander,knowledgeBase):
 """
 13. [0] if (doesn't have flag && enemy flag is captured)
 	 Charge (closest edge map and approach flag spawning point following the side edge)
 """
-def capture_rule13(bot,commander,knowledgeBase):
     if not bot.flag and commander.game.enemyTeam.flag.carrier != None:
         target = commander.game.enemyTeam.flagSpawnLocation
         path = knowledgeBase.createShortFlankingPath(bot.position,target)
@@ -162,11 +176,12 @@ def capture_rule13(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule14(bot,commander,knowledgeBase):
 """
 14. [0] if (doesn't have flag && enemy flag is captured)
 	 Charge (furthest edge map and approach flag spawning point following the side edge)
 """
-def capture_rule14(bot,commander,knowledgeBase):
     if not bot.flag and commander.game.enemyTeam.flag.carrier != None:
         target = commander.game.enemyTeam.flagSpawnLocation
         path = knowledgeBase.createLongFlankingPath(bot.position,target)
@@ -174,21 +189,23 @@ def capture_rule14(bot,commander,knowledgeBase):
         return True
     return False
 	
+
+def capture_rule15(bot,commander,knowledgeBase):
 """
 15. [0] if (doesn't have flag && our flag is captured)
 	 Charge (enemy scoring point)
 """
-def capture_rule15(bot,commander,knowledgeBase):
     if not bot.flag and knowledgeBase.ourFlagCaptured:
         target = commander.game.enemyTeam.flagScoreLocation
         commander.issue(orders.Charge,bot,target, description = "Catcher "+ bot.name + " Charge enemy's scoring point")
         return True
     return False
 	
+
+def default_catcher_rule(bot,commander,knowledgeBase):
 """
 	The default catcher rule.
 """
-def default_catcher_rule(bot,commander,knowledgeBase):
     if not bot.flag:
         target = commander.game.enemyTeam.flag.position
     else:
