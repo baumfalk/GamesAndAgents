7164a93a346ae50527bce971fbdaacb4ef18ec28
Ghostkeeper <ghost_keeper+github@hotmail.com>
2014-06-11 14:27:09 +0200

    Bugfix catcher rule
    One catcher rule gave an exception. Fixed!

--- CaptureTheFlag/catcherRules.py
+++ CaptureTheFlag/catcherRules.py
@@ -140,7 +140,7 @@ def capture_rule14(bot,commander,knowledgeBase):
 
 def capture_rule15(bot,commander,knowledgeBase):
     """ 15. [0] if (doesn't have flag && our flag is captured) Charge (enemy scoring point) """
-    if not bot.flag and (self.commander.game.team.flag.carrier != None):
+    if not bot.flag and commander.game.team.flag.carrier != None:
         target = commander.game.enemyTeam.flagScoreLocation
         commander.issue(orders.Charge,bot,target, description = "Catcher "+ bot.name + " Charge enemy's scoring point")
         return True
